32a3891ba968eddf026e6439a350bdc8ba61af94
pedroxbastos <bastos_pedro@netcabo.pt>
2018-12-15 19:19:36 +0000

    Users update
    Send location and update the DB. Send and receive messages through a queue

--- Server.py
+++ Server.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask,redirect
 from flask import jsonify
 from flask import request
-
+import requests
+import fenixedu
+import pymongo
+from pymongo import MongoClient
+import pprint
 	
-logs = {}		
+Messages = {}		
+client = MongoClient('mongodb://pedroxbastos:Pb9127416716@ds025180.mlab.com:25180/asint')
+db = client.asint
 
-def addLog(name,Location):
-	logs.update({name:Location})
+def addLog(name,location):
+	logs.update({"name":name, "localization": location})
 
 app = Flask(__name__)
 
@@ -31,37 +37,71 @@ def ListUsersInsideB():
 def getUserHistory():
 	return jsonify( {"aaa":12, "bbb": ["bbb", 12, 12] })
 
-@app.route('/API/User/init', methods=['POST'])
-def Getusert():
+"""@app.route('/API/User/init', methods=['POST'])
+def redirectURLforuser():
 	#para receber o token do user (ainda nao funciona)
+	config = fenixedu.FenixEduConfiguration('1977390058176578', 'http://127.0.0.1:5002/User/Mainpage', 'XUwJJfct3cBy7jBbS+NOQVqrQ1eQGN1F9kYAWI3MCMpxgG/J4+h3Qv9+GmOS0Fs+F+Aml8kkZVXroJIb8SZyRw==', 'https://fenix.tecnico.ulisboa.pt/')
+	client = fenixedu.FenixEduClient(config)
+	url = client.get_authentication_url()
+	return jsonify({'url': url})"""
+
+
+@app.route('/API/User/Tokencode', methods=['POST'])
+def getUtoken():
+	config = fenixedu.FenixEduConfiguration('1695915081465925', 'http://127.0.0.1:5002/User/Mainpage', 'd/USUpUYU7o20hWNwEi+S3PWW5Cc4ypiQrX3rUfxLAcFat0epdCuxjS35iIWNwJ4ruCu9D7bL2GXZc9P4RDNvQ==', 'https://fenix.tecnico.ulisboa.pt/')
+	client = fenixedu.FenixEduClient(config)
 	content = request.get_json()
-	print(content["name"])
-	print(content["url"])
-	return jsonify( [{"result": "ACK init"}] )
+	user = client.get_user_by_code(str(content["code"]).split('=')[1])
+	person = client.get_person(user)
+	return jsonify( [{"result": "Token successfull"}] )
+
+
 
 @app.route('/API/User/PostmyLocation', methods=['POST'])
 def PostmyLocal():
 	#User envia localizaçao e nome e atualiza. Isto depois deve atualizar na BD
+	collection = db.logs
 	content = request.get_json()
+	print(content)
 	print(content["name"])
-	print(content["location"])
-	name = content["name"]
-	Location = content["location"]
-	addLog(name,Location)
-	print(logs)
+	localisation={"Latitude": content["location"][0], "Longitude":content["location"][1]}
+	for obj in collection.find({"name": content["name"]}):
+		collection.update_one({"name": content["name"]}, {"$set": {"localisation": localisation}})
+		pprint.pprint(obj)
+		break
 	return jsonify( [{"result": "Logs updated"}] )
 
 @app.route('/API/User/SendBroadMsg', methods=['POST'])
 def SendMsg():
+	collection = db.logs
+	content = request.get_json()
+	objself=None
+	for obj in collection.find({"name": content["name"]}):
+		objself=obj
+		break
+	for obj in collection.find():
+		if obj != objself:
+			if obj["name"] in Messages:
+				Messages[obj["name"]].append(content["Message"])
+			else:
+				Messages[obj["name"]]= []
+				Messages[obj["name"]].append(content["Message"])
+	print(Messages)
 	return jsonify( {"aaa":12, "bbb": ["bbb", 12, 12] })
 
 @app.route('/API/User/DefineDistance', methods=['POST'])
 def DefineRange():
 	return jsonify( {"aaa":12, "bbb": ["bbb", 12, 12] })
 
-@app.route('/API/User/RecvMsg', methods=['GET'])
-def RecvMsg():
-	return jsonify( {"aaa":12, "bbb": ["bbb", 12, 12] })
+@app.route('/API/User/RecvMsg/<idUser>', methods=['GET'])
+def RecvMsg(idUser):
+	collection = db.logs
+	data = {}
+	for key,values in Messages.items():
+		if key == idUser:
+			data[key] = values
+			Messages[key] = []
+	return jsonify(data)
 
 
 if __name__ == '__main__':
--- User.py
+++ User.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import render_template
 import requests
 import fenixedu
+import json
 
 class user:
 	def __init__(self, name):
@@ -11,28 +12,44 @@ class user:
 app = Flask(__name__)
 
 newuser = user(None)
-
+urltrue = None
 @app.route('/')
 def hello_world():
 	return render_template("siteinit.xhtml") 
 
+config = fenixedu.FenixEduConfiguration('1695915081465925', 'http://127.0.0.1:5002/User/Mainpage', 'd/USUpUYU7o20hWNwEi+S3PWW5Cc4ypiQrX3rUfxLAcFat0epdCuxjS35iIWNwJ4ruCu9D7bL2GXZc9P4RDNvQ==', 'https://fenix.tecnico.ulisboa.pt/')
+client = fenixedu.FenixEduClient(config)
+url = client.get_authentication_url()
+
 @app.route('/User/Login')
 def login_fenix():
 	newuser.name = (str(request.args["LoginId"]))
-	print(newuser.name)
-	config = fenixedu.FenixEduConfiguration('1977390058176578', 'http://127.0.0.1:5002/User/Mainpage', 'XUwJJfct3cBy7jBbS+NOQVqrQ1eQGN1F9kYAWI3MCMpxgG/J4+h3Qv9+GmOS0Fs+F+Aml8kkZVXroJIb8SZyRw==', 'https://fenix.tecnico.ulisboa.pt/')
-	client = fenixedu.FenixEduClient(config)
-	url = client.get_authentication_url()
-	server = 'http://127.0.0.1:5000/User/init'
-	print(url)
-	#r = requests.post(server, data={'url': url.split('=')[1]})
 	return redirect(url, code=302)
-
+	
 @app.route('/User/Mainpage')
 def main_user():
-	content = request.get_json()
-	print(content)
 	return render_template("Usertemplate.xhtml", LoginId = str(newuser.name))
+
+@app.route('/User/getToken', methods=['POST'])
+def getToken():
+	content = request.get_json()
+	print(str(content))
+	print(str(content["code"]))
+	print(str(content["code"]).split('=')[1])
+	#url = "http://127.0.0.1:5000/API/User/Tokencode"
+	user = client.get_user_by_code(str(content["code"]).split('=')[1])
+	person = client.get_person(user)
+	print(person)
+	#r = r.json()
+	#print(data)
+	#print(user)
+	#new = json.loads(jsonify(user))
+	#json.dumps(new, indent=4, sort_keys=True)
+	#person = client.get_person(user)
+	#new2 = json.loads(person)	
+	#json.dumps(new2, indent=4, sort_keys=True)
+	return jsonify( [{"result": "Logs updated"}] )
+
 
 
 if __name__ == '__main__':
